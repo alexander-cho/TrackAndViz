dc1d808a51f639c0523b8c0fc640bae6063618be
Alex Cho <alex.jhcho@gmail.com>
2025-01-29 23:57:16 -0800

    deploy service with docker to local minikube cluster

---
+++ help/how-tos.md
@@ -0,0 +1,2 @@
+how to use local docker images in minikube
+https://www.youtube.com/watch?v=7tWJbsGglYA
--- spring_/ch10/src/main/java/dev/vf/ch10/NotEnoughMoneyException.java
+++
@@ -1,5 +0,0 @@
-package dev.vf.ch10;
-
-public class NotEnoughMoneyException extends RuntimeException {
-
-}
